30d75e8e68733ee463164d0aea9e86181f20d443
Mohammed Naji <m.naji1512@gmail.com>
2022-07-23 16:55:48 +0300

    lec13

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class Postcontroller extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        // $posts = Post::all();
+        // $posts = Post::orderByDesc('id')->paginate(20);
+        // $posts = Post::simplePaginate(20);
+        // $posts = Post::latest('id')->get();
+        // $posts = Post::orderBy('id', 'desc')->get();
+        // $posts = Post::orderByDesc('id')->get();
+        // $posts = Post::select('title', 'body')->get();
+        // $posts = Post::select('title', 'body')->take(5)->get();
+        // $posts = Post::find(20);
+        // $posts = Post::where('id', 20)->get();
+        // $posts = Post::where('id', 20)->first();
+
+        // dd($posts);
+
+        // dump(request()->all());
+        $count = 20;
+        if(request()->has('count') && request()->count != 'all') {
+            $count = request()->count;
+        }
+
+        if(request()->has('count') && request()->count == 'all') {
+            $count = Post::count();
+        }
+
+        if(request()->has('search')) {
+            $posts = Post::where('title', 'like', '%' . request()->search . '%')->orWhere('body', 'like', '%' . request()->search . '%')->orderByDesc('id')->paginate($count);
+        }else {
+            $posts = Post::orderByDesc('id')->paginate($count);
+        }
+
+
+
+        return view('posts.index', compact('posts'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,6 +11,19 @@
 <body>
     <div class="container my-5">
         <h1>All Posts</h1>
+        <form action="{{ route('posts.index') }}" method="get">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control w-50" placeholder="Search about anything.." aria-label="Search about anything.." value="{{ request()->search }}" name="search" aria-describedby="Search about anything..">
+                <select name="count" class="form-select w-25">
+                    <option {{ request()->count == 10 ? 'selected' : '' }} value="10">10</option>
+                    <option {{ request()->count == 15 ? 'selected' : '' }} value="15">15</option>
+                    <option {{ request()->count ? '' : 'selected' }} {{ request()->count == 20 ? 'selected' : '' }} value="20">20</option>
+                    <option {{ request()->count == 25 ? 'selected' : '' }} value="25">25</option>
+                    <option {{ request()->count == 'all' ? 'selected' : '' }} value="all">All</option>
+                </select>
+                <button class="btn btn-dark" id="button-addon2">Search</button>
+              </div>
+        </form>
         <table class="table table-bordered table-hover table-striped">
             <tr class="table-dark">
                 <th>ID</th>
@@ -21,61 +34,23 @@
                 <th>Updated At</th>
                 <th>Actions</th>
             </tr>
+            @foreach ($posts as $post)
             <tr>
-                <td>1</td>
-                <td>New Post</td>
-                <td>ffffff</td>
-                <td>100</td>
-                <td>10/10/2010</td>
-                <td>10/10/2020</td>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td><img width="100" src="{{ $post->image }}" alt=""></td>
+                <td>{{ $post->viewer }}</td>
+                <td>{{ $post->created_at->format('M d, Y') }}</td>
+                <td>{{ $post->updated_at->diffForHumans() }}</td>
                 <td>
                     <a class="btn btn-sm btn-primary" href="#"><i class="fas fa-edit"></i></a>
                     <a class="btn btn-sm btn-danger" href="#"><i class="fas fa-trash"></i></a>
                 </td>
             </tr>
-
-            <tr>
-                <td>1</td>
-                <td>New Post</td>
-                <td>ffffff</td>
-                <td>100</td>
-                <td>10/10/2010</td>
-                <td>10/10/2020</td>
-                <td>
-                    <a class="btn btn-sm btn-primary" href="#"><i class="fas fa-edit"></i></a>
-                    <a class="btn btn-sm btn-danger" href="#"><i class="fas fa-trash"></i></a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>New Post</td>
-                <td>ffffff</td>
-                <td>100</td>
-                <td>10/10/2010</td>
-                <td>10/10/2020</td>
-                <td>
-                    <a class="btn btn-sm btn-primary" href="#"><i class="fas fa-edit"></i></a>
-                    <a class="btn btn-sm btn-danger" href="#"><i class="fas fa-trash"></i></a>
-                </td>
-            </tr>
-
-            <tr>
-                <td>1</td>
-                <td>New Post</td>
-                <td>ffffff</td>
-                <td>100</td>
-                <td>10/10/2010</td>
-                <td>10/10/2020</td>
-                <td>
-                    <a class="btn btn-sm btn-primary" href="#"><i class="fas fa-edit"></i></a>
-                    <a class="btn btn-sm btn-danger" href="#"><i class="fas fa-trash"></i></a>
-                </td>
-            </tr>
-
-
-
+            @endforeach
         </table>
+        {{-- {{ $posts->appends(['search' => request()->search, 'count' => request()->count])->links() }} --}}
+        {{ $posts->appends($_GET)->links() }}
         {{-- <i class="fab fa-facebook"></i>
         <i class="fas fa-star"></i>
         <i class="far fa-star"></i> --}}
